66f4724fc901236b185a19e62ade88de5959a749
Yoqub <yyboboraximov@gmail.com>
2022-07-22 06:26:06 +0500

    task2

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println(" I'm\n updated\n can\n be\n compiled.\n And\n can\n write\n to\n console");
     }
 }
